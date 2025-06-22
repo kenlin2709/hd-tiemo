ab22d002d6c152e08850e081f427e4d894a3d5dc
Ken <ken@ausmed.com.au>
2025-06-22 19:58:25 +1000

    added router module to footer.

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  standalone: true,
+  imports: [RouterModule,]
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
